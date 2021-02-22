0b194c03363cd2471ad4bcdd0c2f9d92760e6d87
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-22 00:35:07 -0600

    added multiple setTimeout calls to the getWords function

--- app.js
+++ app.js
@@ -8,5 +8,22 @@ function stringMessage2() {
     }, 2000);
 };
 
+function getWords() {
+    console.log("Word One");
+
+    setTimeout(() => {
+        console.log("Word Two")
+    }, 3000);
+
+    setTimeout(() => {
+        console.log("Word Three")
+    }, 5000);
+
+    setTimeout(() => {
+        console.log("Final Word")
+    }, 6000);
+}
+
 stringMessage1("What time is it...");
 stringMessage2();
+getWords();
